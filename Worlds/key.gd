c8c3fb984cdb2a2cbbd7fc95da5ccd7e3a393a16
Guss <gustavocsj13@gmail.com>
2025-01-31 00:24:56 -0300

    Mensagem key foi pega

--- Worlds/key.gd
+++ Worlds/key.gd
@@ -1,9 +1,26 @@
 extends Area2D
 
+@export var dialogue_name: String = "timeline"
+@onready var key = $sprite_key
+var player_in_range = false
+var dialogo_reproduzido = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_PAUSABLE
 	pass # Replace with function body.
+
+func start_dialogue():
+	if not dialogo_reproduzido:
+		print("Iniciando diálogo...")
+		
+		# Carrega o diálogo pelo nome
+		var dialogue = Dialogic.start(dialogue_name)
+		
+		dialogo_reproduzido = true
+		
+		# Adiciona o diálogo à cena atual
+		add_child(dialogue)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +30,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	body.has_key = true
-	queue_free()
+	start_dialogue()
+	key.visible = false
+	#queue_free()
+	
