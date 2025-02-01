ee15f2c2a4e612b064bee67d59d87ef23286a4dc
Guss <gustavocsj13@gmail.com>
2025-02-01 03:16:54 -0300

    DEscrição com dialogo

--- Tile Maps/estante_3.gd
+++ Tile Maps/estante_3.gd
@@ -1,15 +1,25 @@
 extends Area2D
 var player_in_area := false
-
+@export var dialogue_name: String = "estante3"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func start_dialogue():
+
+	print("Iniciando diálogo...")
+	
+	# Carrega o diálogo pelo nome
+	var dialogue = Dialogic.start(dialogue_name)
+	
+	# Adiciona o diálogo à cena atual
+	add_child(dialogue)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player_in_area and Input.is_action_just_pressed("iteration"):
+		start_dialogue()
 		print("Nao tem nada por aqui 3")
 
 
--- Worlds/door.gd
+++ Worlds/door.gd
@@ -4,6 +4,8 @@ extends Area2D
 @onready var door_close = $Door_closed
 @onready var door_open = $Door_open
 @onready var door_blocker = $Door_blocker/CollisionShape2D   # O StaticBody2D que impede a passagem
+@export var dialogue_name: String = "doorOpen"
+@export var dialogue_name2: String = "door"
 var player_nearby = false
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +14,15 @@ func _ready() -> void:
 	door_open.visible = false
 	door_blocker.set_deferred("disabled", false)
 
+func start_dialogue(dialogue_name):
+
+	print("Iniciando diálogo...")
+	
+	# Carrega o diálogo pelo nome
+	var dialogue = Dialogic.start(dialogue_name)
+	
+	# Adiciona o diálogo à cena atual
+	add_child(dialogue)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -27,8 +38,10 @@ func _process(delta: float) -> void:
 			door_open.visible = true
 			door_blocker.set_deferred("disabled", true)
 			print("Abriu")
+			start_dialogue(dialogue_name)
 		else:
 			print("Precisa da chave")
+			start_dialogue(dialogue_name2)
 
 
 func _on_body_exited(body: Node2D) -> void:
